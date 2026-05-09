352cabeea0e9e2a4ca3f056fc45c1fc8724ad609
MUKTA ROY <muktaroy520@gmail.com>
2026-05-09 16:42:59 +0600

    feat: update font family to Inter and enhance layout structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,21 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Rise Clone",
-  description: "Rise at Seven Homepage Clone",
+  description: "Rise at Seven Clone",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
